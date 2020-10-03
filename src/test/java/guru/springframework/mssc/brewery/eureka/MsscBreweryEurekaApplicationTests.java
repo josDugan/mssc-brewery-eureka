e5c462657cefbff7d40daa4c866267daceb53486
josDugan <josephdug@gmail.com>
2020-10-02 20:31:57 -0400

    Initial commit. Closes #1.

---
+++ src/test/java/guru/springframework/mssc/brewery/eureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc.brewery.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
